a035396d53bb31e82e05eae56b4169f23f72fa4d
Salemofgod <fannellegnan@gmail.com>
2025-12-19 07:12:11 +0300

    Add files via upload

---
+++ todo list rpi lab4/src/view/task-component.js
@@ -0,0 +1,34 @@
+// src/view/task-component.js
+import { createElement } from "../framework/render.js";
+
+function createTaskComponentTemplate(task) {
+  const { title, status } = task;
+  return `
+    <li class="task-item task--${status}">
+      <div class="task-body">
+        <p class="task-title">${title}</p>
+      </div>
+    </li>
+  `;
+}
+
+export default class TaskComponent {
+  constructor(task) {
+    this.task = task;
+  }
+
+  getTemplate() {
+    return createTaskComponentTemplate(this.task);
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
